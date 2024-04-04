51be9775b0af2337fd8bc19a6e5068c253b8c454
Aromal0708 <aromalsunil12@gmail.com>
2024-04-04 20:39:52 +0530

    Login page design done animation yet to come

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react"
-import "./App.css"
-import Home from "./Pages/Home/Home"
+import React from "react";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login/Login";
 
 function App() {
-
   return (
     <>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login"  element={<Login/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./Login.css"
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-body">
+      <div className="login-container">
+        <span className="header">Signup</span>
+          <div className="input">
+          <input placeholder="Enter Email" type="email" className="text-box" />
+          <input placeholder="Enter Password" type="password" className="text-box" />
+          </div>
+          <div className="submit">
+          <button className="dark-button">Login</button>
+          <button className="normal-button">Signup</button>
+          </div>
+            
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
